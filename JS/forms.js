3f07c10a948a59b9ce83390fa150529189fac7c4
FelipeBinatto <felipe.binatto@etec.sp.gov.br>
2025-06-28 19:11:10 -0300

    implementação de botoẽs para aumentar e diminuir o tamanho da fonte

--- JS/forms.js
+++ JS/forms.js
@@ -17,7 +17,18 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 })
+
 document.getElementById('theme-switch').addEventListener('click', function (e) {
-  e.preventDefault(); // impede comportamento padrão
-  document.body.classList.toggle('darkmode');
+    e.preventDefault(); // impede comportamento padrão
+    document.body.classList.toggle('darkmode');
+});
+
+document.getElementById('increase-font').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.classList.add('fonte-maior');
+});
+
+document.getElementById('decrease-font').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.body.classList.remove('fonte-maior');
 });
